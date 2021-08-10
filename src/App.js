bf2346e9d7a5089434e4804eb1449e50d5af9b43
pritamvidhate <30730433+pritamvidhate@users.noreply.github.com>
2021-08-10 19:08:21 +0530

    working on definition of word

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import axios from 'axios';
 import './App.css';
 import {useState, useEffect} from 'react';
-import {Container} from '@material-ui/core';
+import {Container, Switch, withStyles} from '@material-ui/core';
 import Header from './component/Header/Header';
 import Definition from './component/Definitions/Definition';
+import { grey } from '@material-ui/core/colors';
 
 function App() {
   const [word, setWord] = useState("");
   const [meaning, setMeaning] = useState([]);
-  const [category, setCategory] = useState("en")
+  const [category, setCategory] = useState("en");
+
+  const PurpleSwitch = withStyles({
+  switchBase: {
+    color: grey[300],
+    '&$checked': {
+      color: grey[500],
+    },
+    '&$checked + $track': {
+      backgroundColor: grey[500],
+    },
+  },
+  checked: {},
+  track: {},
+})(Switch);
 
   const dictionaryApi = async () =>{
     try {
       const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/${category}/${word}`);  
 
-  console.log(data);
+  // console.log(data);
 
     } catch (error) {
       console.log(error);
--- src/component/Definitions/Definition.js
+++ src/component/Definitions/Definition.js
@@ -3,8 +3,44 @@ import './Definition.css';
 
 const Definition = ({word, category, meaning}) => {
     return (
-        <div>
-            <h1>hello</h1>
+        <div className="meanings">
+
+            {
+                meaning[0] && word && category ==="en" && (
+                    <audio src={meaning[0].phonetic[0] && meaning[0].phonetic[0].audio}
+                        style={{backgroundColor: "#fff", borderRadius: 10}}
+                        controls>
+                        browser does not support audio
+                    </audio>
+                )
+            }
+
+            {word === "" ? (
+                <span className="subTitle">searching the word</span>
+            ):(
+                meaning.map((mean) => mean.meaning.map((item) => 
+                item.definition.map((def) => (
+                    <div className="singleMeN" 
+                        style={{backgroundColor: "white", color: "black"}}
+                    >
+                        <b>{def.definition}</b>
+                            <hr style={{backgroundColor:"black", width:"100%"}}/>
+                            {
+                                def.example && (
+                                    <span>
+                                        <b>Example:</b>
+                                        {def.example}
+                                    </span>
+                                )}
+                                <span> 
+                                    <b>synonyms :</b>
+                                    {def.synonyms.map((s) => `${s}, `)}
+                                </span>
+                    </div>
+                ))
+            )
+        )
+    )}
         </div>
     )
 }
